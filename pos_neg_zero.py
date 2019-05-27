cf6328f8a17581282b7643f583f5f90b19d3db5e
KarthikKavinA <42880791+KarthikKavinA@users.noreply.github.com>
2019-05-27 21:04:38 +0530

    Update pos_neg_zero.py

--- pos_neg_zero.py
+++ pos_neg_zero.py
@@ -1,4 +1,4 @@
-#Program to check a number whether it is positive or negative or zero.
+#Program to check whether a number is positive or negative or zero.
 n=int(input())
 if n==0:
   print("Zero")
