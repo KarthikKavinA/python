240eb42c67660cd95dcc36cf5bcbef11131c8681
KarthikKavinA <42880791+KarthikKavinA@users.noreply.github.com>
2019-05-27 21:09:24 +0530

    Update pos_neg_zero.py

--- pos_neg_zero.py
+++ pos_neg_zero.py
@@ -1,3 +1,5 @@
+#CODEKATA BEGINNER SET-1
+
 #Program to check whether a number is positive or negative or zero.
 n=int(input())
 if n==0:
@@ -6,5 +8,15 @@ elif n>0:
   print("Positive")
 elif n<0:
   print("Negative")
+else:
+  print("Invalid Input")
+  
+  
+#Program to check whether a number is odd or even.
+n=int(input())
+if n%2==0:
+  print("Even")
+elif n%2!=0:
+  print("Odd")
 else:
   print("Invalid Input")
