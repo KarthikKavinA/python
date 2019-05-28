5978d6884e28f2d745a3acf72145a8ef594ddcfb
KarthikKavinA <42880791+KarthikKavinA@users.noreply.github.com>
2019-05-28 14:10:11 +0530

    Update largest_of_3nos.py

--- set-1/largest_of_3nos.py
+++ set-1/largest_of_3nos.py
@@ -1,1 +1,18 @@
-
+#Program to find greatest of three numbers.
+a,b,c=map(int,input().split())
+if a>b:
+  if a>c:
+    print(a)
+  else:
+    print(c)
+elif b>a:
+  if b>c:
+    print(b)
+  else:
+    print(c)
+elif c>a and c>b:
+  print(c)
+else:
+  print("Invalid Input")
+  
+    
