6baeff69969523245617031e7c7fbc701a4bb76b
KarthikKavinA <42880791+KarthikKavinA@users.noreply.github.com>
2019-05-28 13:54:50 +0530

    Update leap_year.py

--- set-1/leap_year.py
+++ set-1/leap_year.py
@@ -1,2 +1,12 @@
 #program to check whether a given year is leap or not?
-n=int(input())
+y=int(input())
+if y%4==0:
+  if y%100!=0:
+    print("yes")
+  else:
+    if y%400==0:
+      print("yes")
+    else:
+      print("no")
+else:
+  print("no")
